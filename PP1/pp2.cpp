6709acd23b0303c7c7d016fcda722ad3451781e5
Jailson Pereira <jailsoncolares@gmail.com>
2017-10-30 16:57:46 -0400

    oremos

--- PP1/pp2.cpp
+++ PP1/pp2.cpp
@@ -473,9 +473,8 @@ void Matriz::mapeaRei(int l,int c,int lr,int cr){
   while(fila.vazia()){
     pair<int,int> cord2;
     cord2 = fila.desenfileira();
-      vector<int> ent; //vector para as entradas
+    vector<int> ent; //vector para as entradas
       for(int i=0;i<=8;i++){
-        //vector<int> ent; //vector para as entradas
         l1 = cord2.first + di[i];
         c1 = cord2.second +dj[i];
         pair<int,int> cord;
@@ -485,19 +484,16 @@ void Matriz::mapeaRei(int l,int c,int lr,int cr){
             fila.enfileira({l1,c1});
             int indice_vertice = tabuleiro_grafo[{l1,c1}];
             ent.push_back(indice_vertice);
-            //g.insertEdge(tabuleiro_grafo[cord2],indice_vertice);
           }
         }
       }
       //Aqui
-      //Ordena ord;
-      quicksort(ent,0,ent.size()-1);
-      //g.insertEdge(tabuleiro_grafo[cord2],ent[i]);
-      for(int j=0;j<ent.size();j++){
-        g.insertEdge(tabuleiro_grafo[cord2],ent[j]);
-      }
-
+    quicksort(ent,0,ent.size()-1);
+    for(int j=0;j<ent.size();j++){
+      g.insertEdge(tabuleiro_grafo[cord2],ent[j]);
     }
+
+  }
 
     g.print();
     int inicio = tabuleiro_grafo[{l,c}];
@@ -508,7 +504,7 @@ void Matriz::mapeaRei(int l,int c,int lr,int cr){
     menor = bfs.getD(bfs.getAchado());
     cout << menor << endl;
     //ext = bfs.getCaminho();
-}
+  }
 
 pair<int,int> Matriz::getGrafoTabuleiro(int i){
   return grafo_tabuleiro[i];
@@ -518,13 +514,126 @@ vector<int> Matriz::getExt(){
   return ext;
 }
 
+//classe entrada e saída
+/*class Processamento{
+private:
+	vector<int> numeros {NIL, 49, 50, 51, 52, 53, 54, 55, 56}; //1,2,3...
+	vector<int> letras {NIL, 97, 98, 99, 100, 101, 102, 103, 104};//a,b,c,d...
+	vector<pair<int, int> > coordenadas;
+	vector<string> entrada; // vector com as posicoes dos cavalos e rei
+  vector<pair<int,int> > minimo;
+  vector<int> menor;
+  Matriz mat[4];
+	string jogada; //posicao de cada cavalo e do rei
+
+public:
+	Processamento();
+	void recebeEntrada();
+	void convertePosicao();
+	pair<string,int> invertePosicao(pair<int,int> );
+  void inicializaMatriz();
+  pair<int,int> getRei();
+  void finaliza();
+  void finalizaMostra();
+};
+
+Processamento::Processamento(){
+}
+
+void Processamento::recebeEntrada(){
+	for(int i = 0; i < 5; ++i){
+		cin >> jogada;
+		cin.ignore();
+		entrada.push_back(jogada);
+	}
+	convertePosicao();
+}
+
+void Processamento::convertePosicao(){
+	pair<int, int> par;
+	for (int i = 0; i < 5; ++i){
+		par = make_pair(entrada[i][1],entrada[i][0]);
+		coordenadas.push_back(par);
+	}
+
+	for (int i = 0; i < coordenadas.size(); ++i){
+		for (int j = 1; j <= numeros.size(); ++j){
+			if(coordenadas[i].first == numeros[j]){
+				coordenadas[i].first=j;
+			}
+		}
+	}
+
+	for (int i = 0; i < coordenadas.size(); ++i){
+		for (int j = 1; j <= letras.size(); ++j){
+			if(coordenadas[i].second == letras[j]){
+				coordenadas[i].second=j;
+			}
+		}
+	}
+
+}
+
+pair<string,int> Processamento::invertePosicao(pair<int,int> p){
+	pair<string, int> par;
+		for (int j = 1; j <= letras.size(); ++j){
+			if(p.second == j){
+				par = make_pair(letras[j],p.first);
+				//saida.push_back(par);
+        return par;
+			}
+		}
+
+}
+
+pair<int,int> Processamento::getRei(){
+  return coordenadas[4];
+}
+
+void Processamento::inicializaMatriz(){
+  pair<int,int> rei = getRei();
+  for(int i=0;i<4;i++){
+    mat[i].inicializa();
+    mat[i].preenche();
+    mat[i].mapeaRei(coordenadas[i].first,coordenadas[i].second,rei.first,rei.second);
+    minimo.push_back({mat[i].getMenor(),i});
+  }
+}
+
+void Processamento::finaliza(){
+
+  int aux = minimo[0].first;
+  int it;
+  for(int i=1;i<=minimo.size()-1;i++ ){
+    if(minimo[i].first < aux ){
+        aux = minimo[i].first;
+        it = i;
+    }
+  }
+  menor.push_back(minimo[it].second);
+  for(int j=0;j<minimo.size();j++){
+    if(j != it){
+      if(aux == minimo[j].first){
+        menor.push_back(minimo[j].second);
+      }
+    }
+  }
+}
+
+void Processamento::finalizaMostra(){
+  for(int i =0;i<menor.size();i++){
+     cout << mat[menor[i]].getMenor() << '\t';
+     for(int j=mat[menor[i]].getExt().size()-1; j>=0 ; j--){
+      pair<int,int> p = mat[menor[i]].getGrafoTabuleiro(mat[menor[i]].getExt()[j]);
+      pair<string,int> q = invertePosicao(p);
+      cout << q.first << q.second << '\t';
+     }
+     cout << endl;
+  }
+}
+*/
+
 int main(int argc, char const *argv[]) {
-  /*Grafo g(5);
-  cout << "-----grafo-----" << endl;
-  testeGrafo(g);
-  BFS bfs;
-  bfs.run(g,2,5);
-  bfs.mostraBFS();*/
 
   Matriz mat;
   mat.inicializa();
